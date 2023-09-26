dfc4304aa51083f1279fe2eb96020cd6e6a7995b
Fátima <ffernandezp22@fpcoruna.afundacion.org>
2023-09-26 10:18:43 +0200

    Ejercicio 1 python

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,12 @@
+print ("Para ser más elegante no usa guante ni chaqué solo cambia en un instante por una 'efe' la 'ge': ")
+print ("a. Gato.")
+print ("b. Elefante.")
+print ("c. León.")
+print("")
+
+respuesta = input("Introduce tu respuesta (a/b/c): ")
+
+if respuesta == "b": 
+    print("¡Has acertado!")
+else: 
+    print ("No has acertado :(")
