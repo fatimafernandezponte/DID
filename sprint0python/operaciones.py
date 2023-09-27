04f293c90af769eafd9878a246e4a60e8fc431a6
Fátima <ffernandezp22@fpcoruna.afundacion.org>
2023-09-27 09:08:05 +0200

    Ejercicio 3 python

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,19 @@
+print("OPERACIONES")
+
+def suma (a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+def multiplicacion(a,b):
+    return a*b
+
+def division(a,b):
+    if b == 0:
+        print("Segundo número no válido")
+    else:
+        return a/b
+
+        
+        
